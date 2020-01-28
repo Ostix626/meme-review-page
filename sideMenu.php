53c32292b71a34558d91379ca8804f6b5fd2d1c7
Ostix626 <35279408+Ostix626@users.noreply.github.com>
2020-01-28 23:56:27 +0100

    Add files via upload

--- sideMenu.php
+++ sideMenu.php
@@ -1,5 +1,5 @@
-      <li><a href="#">Fresh memes</a></li>
-      <li><a href="#">Meme of the decade</a></li>
+      <li><a href = "index.php">Fresh memes</a></li>
+      <li><a href="mod.php">Meme of the decade</a></li>
       <li><a href="https://www.youtube.com/watch?v=tJWChYAU5vk&list=PLYH8WvNV1YEn_iiBMZiZ2aWugQfN1qVfM" target="_blank">Meme review</a></li>
       <li><a href="#">Dead memes (RIP)</a></li>
-      <li><a href="#">Top 10 memes</a></li>
+      <li><a href="top10.php">Top 10 memes</a></li>
